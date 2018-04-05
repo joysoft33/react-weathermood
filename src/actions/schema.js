dbdd430f7879ed14f5dd8ce260bdcb5a3586ca96
Eric LEON <ericp.leon@gmail.com>
2018-04-05 14:34:29 +0200

    Remove unused packages

--- src/actions/schema.js
+++
@@ -1,9 +0,0 @@
-import { schema } from 'normalizr';
-
-export const weather = new schema.Entity('weather');
-
-export const playlist = new schema.Entity('playlist');
-export const arrayOfPlaylists = new schema.Array(playlist);
-
-export const track = new schema.Entity('track');
-export const arrayOfTracks = new schema.Array(track);
